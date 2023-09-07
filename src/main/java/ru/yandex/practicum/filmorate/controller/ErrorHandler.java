891168c32755fa90275346e29c93c8bd6a3fc7e7
Amir <amri19040268@gmail.com>
2023-09-07 14:45:34 +0300

    добавлены логи при возникновении ошибок

--- src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
@@ -1,5 +1,7 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -11,10 +13,12 @@ import ru.yandex.practicum.filmorate.model.ErrorResponse;
 @RestControllerAdvice
 public class ErrorHandler {
 
+    private final Logger log = LoggerFactory.getLogger(FilmController.class);
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse handleNotFoundException(final NotFoundException e) {
+        log.debug("Получен статус 404 Not found {}", e.getMessage(), e);
         return new ErrorResponse(
                 e.getMessage()
         );
@@ -23,6 +27,7 @@ public class ErrorHandler {
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleValidationException(final ValidationException e) {
+        log.debug("Получен статус 400 Bad Request {}", e.getMessage(), e);
         return new ErrorResponse(
                 e.getMessage()
         );
