b7bee0665ecc123b96d09ac4e0f45f92eaddaf52
Amir <amri19040268@gmail.com>
2023-09-18 14:13:41 +0300

    изменены структура модуля из-за изменения ТЗ

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,23 +1,29 @@
 package ru.yandex.practicum.filmorate.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
-import java.time.LocalDate;
+import java.sql.Date;
+import java.util.List;
 import java.util.Set;
 
 @Data
+@Builder
+@AllArgsConstructor
 public class Film {
     private Long id;
     @NotBlank
     private String name;
     @NotBlank
     private String description;
-    private LocalDate releaseDate;
+    private Date releaseDate;
     @Min(value = 1)
     private int duration;
+    private int rate;
+    private Mpa mpa;
+    private List<Genre>  genres;
     private Set<Long> like;
-    private long genreId;
-    private long МРАId;
 }
